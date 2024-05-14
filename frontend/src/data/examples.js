d9e81701dbf4151ebd5d01eedb1d4b4704bc12ac
Dhananjhay <dhananjhay03@gmail.com>
2024-05-14 23:08:59 +0530

    changed the structure since we are no longer using reserve parameters

--- frontend/src/data/examples.js
+++ frontend/src/data/examples.js
@@ -7,11 +7,6 @@ export const examplesData = [
       delta: '',
       epsilon: ''
     },
-    reserve_parameters: {
-      a: false,
-      p: false,
-      P: false,
-    },
     metric_tensor: [['r**2',0], [0,'(r**2)*sin(x)**2']],
     onlyCS : 'option_1'
     },
@@ -22,11 +17,6 @@ export const examplesData = [
       alpha: '',
       delta: '',
       epsilon: ''
-    },
-    reserve_parameters: {
-      a: false,
-      p: false,
-      P: false,
     },
     metric_tensor: [['1',0,0], [0,'(x**2)',0],[0,0,'(x**2)*sin(y)**2']],
     onlyCS: 'option_1'
@@ -39,11 +29,6 @@ export const examplesData = [
       delta: '',
       epsilon: ''
     },
-    reserve_parameters: {
-      a: false,
-      p: false,
-      P: false,
-    },
     metric_tensor: [['-(1-r_s/x)',0,0,0], [0,'(1-r_s/x)**(-1)',0,0], [0,0,'x**2',0], [0,0,0,'(x**2)*sin(y)**2']],
     onlyCS: 'option_1'
     },
@@ -54,11 +39,6 @@ export const examplesData = [
       alpha: '',
       delta: '',
       epsilon: ''
-    },
-    reserve_parameters: {
-      a: true,
-      p: false,
-      P: false,
     },
     metric_tensor: [['-1',0,0,0], [0,'(a(t)**2)/(1-(k*x**2))',0,0], [0,0,'a(t)**2*(x**2)',0], [0,0,0,'(a(t)**2)*(x**2)*sin(y)**2']],
     onlyCS: 'option_1'
@@ -71,11 +51,6 @@ export const examplesData = [
       delta: '',
       epsilon: ''
     },
-    reserve_parameters: {
-      a: false,
-      p: false,
-      P: false,
-    },
     metric_tensor: [['-(1 - (r_s/x) + (r_Q**2)/x**2)',0,0,0], [0,'(1 - (r_s/x) + (r_Q**2)/x**2)**-1',0,0], [0,0,'(x**2)',0], [0,0,0,'(x**2)*sin(y)**2']],
     onlyCS: 'option_1'
     },
@@ -87,11 +62,6 @@ export const examplesData = [
       delta: 'x**2 + (a**2)*(cos(y)**2)',
       epsilon: 'x**2 + a**2 - r_s*x'
     },
-    reserve_parameters: {
-      a: false,
-      p: false,
-      P: false,
-    },
     metric_tensor: [['-(1 - (r_s*x)/delta)',0,0,'-(r_s*x*a*sin(y)**2)/delta'], [0,'delta/epsilon',0,0], [0,0,'delta',0], ['-(r_s*a*x*sin(y)**2)/delta',0,0,'(alpha + (r_s*x*(a**2)*sin(y)**2)/delta)*sin(y)**2']],
     onlyCS: 'option_1'
     },
@@ -102,11 +72,6 @@ export const examplesData = [
       alpha: '-(G*M)/x',
       delta: '',
       epsilon: ''
-    },
-    reserve_parameters: {
-      a: false,
-      p: false,
-      P: false,
     },
     metric_tensor: [['-(1 + (2*alpha)/(c**2))',0,0,0], [0,'1- (2*alpha)/(c**2)',0,0], [0,0,'(1 - (2*alpha)/c**2)*(x**2)',0], [0,0,0,'(1-(2*alpha)/(c**2))*(x**2)*(sin(y)**2)']],
     onlyCS: 'option_1'
