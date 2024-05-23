5ffb5f31e084ae55a36702eef8c6576c855b9ae4
Dhananjhay <dhananjhay03@gmail.com>
2024-05-23 09:43:16 +0530

    modified to fit the myinitialvalues structure

--- frontend/src/data/examples.js
+++ frontend/src/data/examples.js
@@ -1,90 +1,138 @@
 export const examplesData = [
      {
     value: "example-1",
-    num_coordinates: 2,
+    coordinates: {
+      num_coordinates: 2,
+      coordinate0: 'theta',
+      coordinate1: 'phi',
+      coordinate2: '',
+      coordinate3: ''
+    },
     variable_parameters: {
       alpha: '',
       delta: '',
       epsilon: ''
     },
-    metric_tensor: [['r**2',0], [0,'(r**2)*sin(x)**2']],
+    metric_tensor: [['r**2',0], [0,'(r**2)*sin(theta)**2']],
     onlyCS : 'option_1'
     },
     {
     value: "example-2",
-    num_coordinates: 3,
+    coordinates: {
+      num_coordinates: 3,
+      coordinate0: 'r',
+      coordinate1: 'theta',
+      coordinate2: 'phi',
+      coordinate3: ''
+    },
     variable_parameters: {
       alpha: '',
       delta: '',
       epsilon: ''
     },
-    metric_tensor: [['1',0,0], [0,'(x**2)',0],[0,0,'(x**2)*sin(y)**2']],
+    metric_tensor: [['1',0,0], [0,'(r**2)',0],[0,0,'(r**2)*sin(theta)**2']],
     onlyCS: 'option_1'
     },
     {
     value: "example-3",
-    num_coordinates: 4,
+    coordinates: {
+      num_coordinates: 4,
+      coordinate0: 't',
+      coordinate1: 'r',
+      coordinate2: 'theta',
+      coordinate3: 'phi'
+    },
     variable_parameters: {
       alpha: '',
       delta: '',
       epsilon: ''
     },
-    metric_tensor: [['-(1-r_s/x)',0,0,0], [0,'(1-r_s/x)**(-1)',0,0], [0,0,'x**2',0], [0,0,0,'(x**2)*sin(y)**2']],
+    metric_tensor: [['-(1-r_s/r)',0,0,0], [0,'(1-r_s/r)**(-1)',0,0], [0,0,'r**2',0], [0,0,0,'(r**2)*sin(theta)**2']],
     onlyCS: 'option_1'
     },
     {
     value: "example-4",
-    num_coordinates: 4,
+     coordinates: {
+      num_coordinates: 4,
+      coordinate0: 't',
+      coordinate1: 'r',
+      coordinate2: 'theta',
+      coordinate3: 'phi'
+    },
     variable_parameters: {
       alpha: '',
       delta: '',
       epsilon: ''
     },
-    metric_tensor: [['-1',0,0,0], [0,'(a(t)**2)/(1-(k*x**2))',0,0], [0,0,'a(t)**2*(x**2)',0], [0,0,0,'(a(t)**2)*(x**2)*sin(y)**2']],
+    metric_tensor: [['-1',0,0,0], [0,'(a(t)**2)/(1-(k*r**2))',0,0], [0,0,'a(t)**2*(r**2)',0], [0,0,0,'(a(t)**2)*(r**2)*sin(theta)**2']],
     onlyCS: 'option_1'
     },
     {
     value: "example-5",
-    num_coordinates: 4,
+     coordinates: {
+      num_coordinates: 4,
+      coordinate0: 't',
+      coordinate1: 'r',
+      coordinate2: 'theta',
+      coordinate3: 'phi'
+    },
     variable_parameters: {
       alpha: '',
       delta: '',
       epsilon: ''
     },
-    metric_tensor: [['-(1 - (r_s/x) + (r_Q**2)/x**2)',0,0,0], [0,'(1 - (r_s/x) + (r_Q**2)/x**2)**-1',0,0], [0,0,'(x**2)',0], [0,0,0,'(x**2)*sin(y)**2']],
+    metric_tensor: [['-(1 - (r_s/r) + (r_Q**2)/r**2)',0,0,0], [0,'(1 - (r_s/r) + (r_Q**2)/r**2)**-1',0,0], [0,0,'(r**2)',0], [0,0,0,'(r**2)*sin(theta)**2']],
     onlyCS: 'option_1'
     },
     {
     value: "example-6",
-    num_coordinates: 4,
-    variable_parameters: {
-      alpha: 'x**2 + a**2',
-      delta: 'x**2 + (a**2)*(cos(y)**2)',
-      epsilon: 'x**2 + a**2 - r_s*x'
+     coordinates: {
+      num_coordinates: 4,
+      coordinate0: 't',
+      coordinate1: 'r',
+      coordinate2: 'theta',
+      coordinate3: 'phi'
     },
-    metric_tensor: [['-(1 - (r_s*x)/delta)',0,0,'-(r_s*x*a*sin(y)**2)/delta'], [0,'delta/epsilon',0,0], [0,0,'delta',0], ['-(r_s*a*x*sin(y)**2)/delta',0,0,'(alpha + (r_s*x*(a**2)*sin(y)**2)/delta)*sin(y)**2']],
+    variable_parameters: {
+      alpha: 'r**2 + a**2',
+      delta: 'r**2 + (a**2)*(cos(theta)**2)',
+      epsilon: 'r**2 + a**2 - r_s*r'
+    },
+    metric_tensor: [['-(1 - (r_s*r)/delta)',0,0,'-(r_s*r*a*sin(theta)**2)/delta'], [0,'delta/epsilon',0,0], [0,0,'delta',0], ['-(r_s*a*r*sin(theta)**2)/delta',0,0,'(alpha + (r_s*r*(a**2)*sin(theta)**2)/delta)*sin(theta)**2']],
     onlyCS: 'option_1'
     },
     {
     value: "example-7",
-    num_coordinates: 4,
+     coordinates: {
+      num_coordinates: 4,
+      coordinate0: 't',
+      coordinate1: 'r',
+      coordinate2: 'theta',
+      coordinate3: 'phi'
+    },
     variable_parameters: {
-      alpha: '-(G*M)/x',
+      alpha: '-(G*M)/r',
       delta: '',
       epsilon: ''
     },
-    metric_tensor: [['-(1 + (2*alpha)/(c**2))',0,0,0], [0,'1- (2*alpha)/(c**2)',0,0], [0,0,'(1 - (2*alpha)/c**2)*(x**2)',0], [0,0,0,'(1-(2*alpha)/(c**2))*(x**2)*(sin(y)**2)']],
+    metric_tensor: [['-(1 + (2*alpha)/(c**2))',0,0,0], [0,'1- (2*alpha)/(c**2)',0,0], [0,0,'(1 - (2*alpha)/c**2)*(r**2)',0], [0,0,0,'(1-(2*alpha)/(c**2))*(r**2)*(sin(theta)**2)']],
     onlyCS: 'option_1'
     },
     {
     value: "example-8",
-    num_coordinates: 4,
+     coordinates: {
+      num_coordinates: 4,
+      coordinate0: 't',
+      coordinate1: 'r',
+      coordinate2: 'theta',
+      coordinate3: 'phi'
+    },
     variable_parameters: {
       alpha: '',
       delta: '',
       epsilon: ''
     },
-    metric_tensor: [['rho(t)',0,0,0], [0,'((-k*x**2+ 1)*P(t))/a(t)**2',0,0], [0,0,'P(t)/(x**2*a(t)**2)',0], [0,0,0,'P(t)/(x**2*a(t)**2*sin(y)**2)']],
+    metric_tensor: [['rho(t)',0,0,0], [0,'((-k*r**2+ 1)*P(t))/a(t)**2',0,0], [0,0,'P(t)/(r**2*a(t)**2)',0], [0,0,0,'P(t)/(r**2*a(t)**2*sin(theta)**2)']],
     onlyCS: 'option_1'
     },
 ];
