abf175a289c2f84d5b21c14204b206fc83bbcba7
Dhananjhay <dhananjhay03@gmail.com>
2024-05-23 09:42:04 +0530

    Reduced redundant code by creating components

--- frontend/src/components/Input/VariableParameters.js
+++ frontend/src/components/Input/VariableParameters.js
@@ -1,74 +1,68 @@
 import React from 'react';
-import { CommonTextField } from '../CommonFormElements';
+import { CommonTextField, coordObjectToList } from '../CommonFormElements';
 import Box from '@mui/material/Box';
 import WarningIcon from '@mui/icons-material/Warning';
 
-const CoordList = ({numCoords}) => {
-
-  const coordList = ['t','x','y','z'];
-  var displayList = coordList.splice(0,numCoords);
-  
-  if(numCoords !== 4){
-    displayList = ['t','x','y','z'].splice(1,numCoords);
-  }
-
-
+const Variable = ({ symbol, name, coordList, placeholder, myInitialValues }) => {
   return (
-    displayList.map((coord, index)=>
-    index === numCoords - 1
-    ?
-    coord
-    :
-    coord + ','
-    )
+    <span className='variable'>
+
+      {symbol} ({
+        coordList.map((coord, index) =>
+          index === coordList.length - 1
+            ?
+            coord
+            :
+            coord + ','
+        )
+      })
+      =
+      <CommonTextField
+        name={"variable_parameters." + name}
+        value={myInitialValues.variable_parameters[name]}
+        placeholder={placeholder}
+      />
+    </span>
   )
 }
 
-const VariableParameters = ({myInitialValues}) => {
+const VariableParameters = ({ myInitialValues }) => {
+
+  var coordList = coordObjectToList(myInitialValues.coordinates)
+
   return (
-         <div className='rightPanel'>
-        <article className='panelHeading'>
+    <>
+      <article className='panelHeading'>
         Define Variable Parameters
-        </article>
-        <Box style={{
-          display: 'flex',
-          flexDirection: 'column',
-          gap: '2rem'
-        }}>
-          <span className='variable'>
-            
-          &#120572; (<CoordList numCoords={myInitialValues.num_coordinates}/>)
-          = 
-          <CommonTextField
-            name="variable_parameters.alpha"
-            value={myInitialValues.variable_parameters.alpha}
-            placeholder={"Example: sin(pi*x) + e**y"}
-          />
-          </span>
-
-          <span className='variable'> 
-          <article>
-          &delta; (<CoordList numCoords={myInitialValues.num_coordinates}/>)
-          = 
-          </article>
-          <CommonTextField
-            name="variable_parameters.delta"
-            value={myInitialValues.variable_parameters.delta}
-            placeholder={"Example: z**(1/2) + abs(t)"}
-          />
-          </span>
-
-          <span className='variable'>
-          &#949; (<CoordList numCoords={myInitialValues.num_coordinates}/>)
-          =
-          <CommonTextField
-            name="variable_parameters.epsilon"
-            value={myInitialValues.variable_parameters.epsilon}
-            placeholder={"Example: log10(x**10)"}
-          />
-          </span>
-        </Box>
-             <article style={{
+      </article>
+      <Box style={{
+        display: 'flex',
+        flexDirection: 'column',
+        gap: '2rem'
+      }}>
+        <Variable
+          symbol={'\u03B1'}
+          name='alpha'
+          coordList={coordList}
+          myInitialValues={myInitialValues}
+          placeholder={'Example: sin(theta)**2 '}
+        />
+        <Variable
+          symbol={'\u03B4'}
+          name='delta'
+          coordList={coordList}
+          myInitialValues={myInitialValues}
+          placeholder={'Example: e**(1/2) + abs(t) '}
+        />
+        <Variable
+          symbol={'\u03B5'}
+          name='epsilon'
+          coordList={coordList}
+          myInitialValues={myInitialValues}
+          placeholder={'Example: pi*log(phi)**2 '}
+        />
+      </Box>
+      <article style={{
         marginTop: '0.8rem',
         display: 'flex',
         alignItems: 'center',
@@ -76,18 +70,18 @@ const VariableParameters = ({myInitialValues}) => {
         fontFamily: 'Roboto',
         textWrap: 'wrap',
       }}>
-      <WarningIcon sx={{
-        color: 'red',
-        fontSize: '1.4rem'
-      }}/> 
-      <span style={{
+        <WarningIcon sx={{
+          color: 'red',
+          fontSize: '1.4rem'
+        }} />
+        <span style={{
           fontSize: '1.2rem',
-          marginTop:'0.1rem'
-      }}>
-        Use <b>Python Syntax</b> and <b>Mathematical Operators</b>.
+          marginTop: '0.1rem'
+        }}>
+          Use <b>Python Syntax</b> and <b>Mathematical Operators</b>.
         </span>
       </article>
-      </div>
+    </>
   )
 }
 
