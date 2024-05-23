6ea574af5b33a04cccec033d3cd8c1602dce49a4
Dhananjhay <dhananjhay03@gmail.com>
2024-05-23 14:30:15 +0530

    fixed a bug that was causing descrepancies in showing coordinates in variable parameters and not clearning previous coordinates object state

--- frontend/src/components/CommonFormElements.js
+++ frontend/src/components/CommonFormElements.js
@@ -14,8 +14,8 @@ import '../App.css'
 export function coordObjectToList(coordObject) {
 
   var dummyList = [];
-  Object.keys(coordObject).map((key)=>{
-    if(coordObject[key] !== '' && typeof coordObject[key] !== 'number'){
+  Object.keys(coordObject).map((key) => {
+    if (coordObject[key] !== '' && typeof coordObject[key] !== 'number') {
       dummyList.push(coordObject[key]);
     }
   })
@@ -26,13 +26,13 @@ export function coordObjectToList(coordObject) {
 function createMatrix(numCoords) {
 
   var dummyList = [];
-  for(let row = 0; row < numCoords; row++){
+  for (let row = 0; row < numCoords; row++) {
     var arr = [];
-    for (let col = 0; col < numCoords; col++){
-        arr.push(0);
+    for (let col = 0; col < numCoords; col++) {
+      arr.push(0);
     }
     dummyList.push(arr);
-}
+  }
 
   return dummyList;
 }
@@ -58,172 +58,192 @@ export const styleButton = {
 };
 
 
-export const CommonTextField = ({placeholder, label, disabled=false, required=false, fullWidth=false, ...props}) => {
+export const CommonTextField = ({ placeholder, label, disabled = false, required = false, fullWidth = false, ...props }) => {
 
-    const [field, meta] = useField(props);
-    const errorText = meta.error || meta.touched ? meta.error : ''
+  const [field, meta] = useField(props);
+  const errorText = meta.error || meta.touched ? meta.error : ''
 
-    return (
-        <Field
-        inputProps={{
-          style: {
-            fontFamily: "Roboto,sans-serif",
-            fontSize: '1.5rem',
-            fontWeight: '200',
-            backgroundColor: 'white'
-          }
-        }}
-        placeholder={placeholder}
-        label={label}
-        as={TextField}
-        type="input"
-        fullWidth={fullWidth}
-        disabled={disabled}
-        required={required}
-        helperText = {errorText}
-        error={!!errorText}
-        {...field}
-        />
-    );
+  return (
+    <Field
+      inputProps={{
+        style: {
+          fontFamily: "Roboto,sans-serif",
+          fontSize: '1.5rem',
+          fontWeight: '200',
+          backgroundColor: 'white'
+        }
+      }}
+      placeholder={placeholder}
+      label={label}
+      as={TextField}
+      type="input"
+      fullWidth={fullWidth}
+      disabled={disabled}
+      required={required}
+      helperText={errorText}
+      error={!!errorText}
+      {...field}
+    />
+  );
 };
 
-    
-export const CommonSelect = ({options, label, ...props}) => {
-        
-        const [field] = useField(props);
-        const { setFieldValue } = useFormikContext();
-        
-        return (
-            <Box 
-            sx={{ 
-              minWidth: 120, 
-              padding: '1rem',
-              backgroundColor: 'white'
-              }}>
-          <FormControl fullWidth>
-            <InputLabel>{label}</InputLabel>
-            <Select
-                {...field}
-                onChange={(e)=>{
-                  setFieldValue(field.name, e.target.value);
-                  setFieldValue('metric_tensor', createMatrix(e.target.value))
-                }
-                }
-                label={label}
-                autoWidth
-                >
-              {options.map((option)=>{
-                  return(
-                      <MenuItem value={option} key={option}>{option}</MenuItem>
-                      )
-                    })}
-            </Select>
-          </FormControl>
-      </Box>
+
+export const CommonSelect = ({ options, label, ...props }) => {
+
+  const [field] = useField(props);
+  const { setFieldValue } = useFormikContext();
+
+  return (
+    <Box
+      sx={{
+        minWidth: 120,
+        padding: '1rem',
+        backgroundColor: 'white'
+      }}>
+      <FormControl fullWidth>
+        <InputLabel>{label}</InputLabel>
+        <Select
+          {...field}
+          onChange={(e) => {
+            setFieldValue(field.name, e.target.value);
+            setFieldValue('metric_tensor', createMatrix(e.target.value))
+            switch (e.target.value) {
+              case 2:
+                setFieldValue('coordinates.coordinate0', 'r')
+                setFieldValue('coordinates.coordinate1', 'theta')
+                setFieldValue('coordinates.coordinate2', '')
+                setFieldValue('coordinates.coordinate3', '')
+                break;
+              case 3:
+                setFieldValue('coordinates.coordinate0', 'r')
+                setFieldValue('coordinates.coordinate1', 'theta')
+                setFieldValue('coordinates.coordinate2', 'phi')
+                setFieldValue('coordinates.coordinate3', '')
+                break;
+              case 4:
+                setFieldValue('coordinates.coordinate0', 't')
+                setFieldValue('coordinates.coordinate1', 'r')
+                setFieldValue('coordinates.coordinate2', 'theta')
+                setFieldValue('coordinates.coordinate3', 'phi')
+                break;
+            }
+          }
+          }
+          label={label}
+          autoWidth
+        >
+          {options.map((option) => {
+            return (
+              <MenuItem value={option} key={option}>{option}</MenuItem>
+            )
+          })}
+        </Select>
+      </FormControl>
+    </Box>
   )
-  
+
 }
 
-export const MatrixComponent = ({placeholder, fullWidth=true, ...props}) => {
+export const MatrixComponent = ({ placeholder, fullWidth = true, ...props }) => {
 
-    const [field, meta] = useField(props);
-    const errorText = meta.error || meta.touched ? meta.error : ''
+  const [field, meta] = useField(props);
+  const errorText = meta.error || meta.touched ? meta.error : ''
 
-    return (
-        <Field
-        fullWidth={fullWidth}
-        inputProps={{
-          style: {
-            fontFamily: "Roboto,sans-serif",
-            fontSize: '1.5rem',
-            fontWeight: '200',
-          }
-        }}
-        placeholder={placeholder}
-        as={TextField}
-        type="input"
-        helperText = {errorText}
-        error={!!errorText}
-        {...field}
-        />
-    );
+  return (
+    <Field
+      fullWidth={fullWidth}
+      inputProps={{
+        style: {
+          fontFamily: "Roboto,sans-serif",
+          fontSize: '1.5rem',
+          fontWeight: '200',
+        }
+      }}
+      placeholder={placeholder}
+      as={TextField}
+      type="input"
+      helperText={errorText}
+      error={!!errorText}
+      {...field}
+    />
+  );
 };
 
-export const CalculateButton = ({isSubmitting, isValid}) => {
+export const CalculateButton = ({ isSubmitting, isValid }) => {
   return (
     <LoadingButton
-    type="Submit"
-    disabled={isSubmitting || !isValid}
-    size="large"
-    variant="contained"
-    style={{ 
-    fontSize: 24, 
-    borderRadius: '1rem',
-    }}
-    loading={isSubmitting}
-    loadingIndicator="Calculating..."
-    sx={styleButton}
+      type="Submit"
+      disabled={isSubmitting || !isValid}
+      size="large"
+      variant="contained"
+      style={{
+        fontSize: 24,
+        borderRadius: '1rem',
+      }}
+      loading={isSubmitting}
+      loadingIndicator="Calculating..."
+      sx={styleButton}
     >
-    Calculate
+      Calculate
     </LoadingButton>
   )
 }
 
-export const CommonAlertDialog = ({name, description, open, handleClose, handleClickOpen}) => {
+export const CommonAlertDialog = ({ name, description, open, handleClose, handleClickOpen }) => {
 
   return (
     <React.Fragment>
       <Button
-      variant='outlined'
-      sx={styleButton}
-      onClick={() => handleClickOpen(name)}
+        variant='outlined'
+        sx={styleButton}
+        onClick={() => handleClickOpen(name)}
       >
         {name}
       </Button>
-    <Dialog
-    open={open}
-    TransitionComponent={Transition}
-    keepMounted
-    onClose={handleClose}
-    maxWidth={'md'}
-    >
-      <DialogTitle sx={{textAlign: 'center'}}>
-        {name}
-      </DialogTitle>
-      <DialogContent>
-        {/* span because DialogContextText is a wrapper over p tag and FormControl is a wrapper over div */}
-        <DialogContentText component={'span'}>
-          {description()}
-        </DialogContentText>
-      </DialogContent>
-      <DialogActions>
-        <Button onClick={handleClose}>Done</Button>
-      </DialogActions>
-    </Dialog>
+      <Dialog
+        open={open}
+        TransitionComponent={Transition}
+        keepMounted
+        onClose={handleClose}
+        maxWidth={'md'}
+      >
+        <DialogTitle sx={{ textAlign: 'center' }}>
+          {name}
+        </DialogTitle>
+        <DialogContent>
+          {/* span because DialogContextText is a wrapper over p tag and FormControl is a wrapper over div */}
+          <DialogContentText component={'span'}>
+            {description()}
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose}>Done</Button>
+        </DialogActions>
+      </Dialog>
     </React.Fragment>
   );
 }
 
-export const AlertError = ({isError, setIsError, errorMessage, setErrorMessage}) => {
+export const AlertError = ({ isError, setIsError, errorMessage, setErrorMessage }) => {
   return (
     <Snackbar
       open={isError}
       autoHideDuration={3000}
-      onClose={()=>{
+      onClose={() => {
         setIsError(false);
         setErrorMessage("");
       }}
-      >
+    >
       <Alert
         severity="error"
-        onClose={()=>{
+        onClose={() => {
           setIsError(false);
           setErrorMessage("")
         }}
       >
         <AlertTitle>
-          Error</AlertTitle> 
-        {errorMessage} <br/>
+          Error</AlertTitle>
+        {errorMessage} <br />
         Revise your inputs and submit a new request.
       </Alert>
     </Snackbar>
