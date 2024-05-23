f0e62e8fe03d2f1727c048d86185f57de55f96c6
Dhananjhay <dhananjhay03@gmail.com>
2024-05-23 09:41:33 +0530

    added new section DefineParameters

--- frontend/src/components/Input/Parameters.js
+++ frontend/src/components/Input/Parameters.js
@@ -1,23 +1,27 @@
 import React from 'react'
-import ReserveParameters from './ReserveParameters';
 import VariableParameters from './VariableParameters';
 import Dimensions from './Dimensions';
+import DefineCoordinates from './DefineCoordinates';
 
 
 
-const Parameters = ({myInitialValues}) => {
+const Parameters = ({ myInitialValues }) => {
 
   return (
     <>
       <div style={{
-          display: 'flex',
-          gap: '1rem',
-        }}>
-          
-      <div className='leftPanel'>
-        <Dimensions myInitialValues={myInitialValues}/>
-      </div>
-          <VariableParameters myInitialValues={myInitialValues}/>
+        display: 'flex',
+        gap: '1rem',
+      }}>
+
+        <div className='leftPanel'>
+          <Dimensions myInitialValues={myInitialValues} />
+          <DefineCoordinates myInitialValues={myInitialValues
+          } />
+        </div>
+        <div className='rightPanel'>
+          <VariableParameters myInitialValues={myInitialValues} />
+        </div>
       </div>
     </>
   )
